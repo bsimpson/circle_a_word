b07ab5505c2a4560cf3a4f114f27763e37381f18
Ben Simpson <thehoagie@gmail.com>
2020-09-06 18:17:37 -0400

    Try and get port from Netlify

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;;
 
 app.set('view engine', 'pug');
 
@@ -14,6 +14,6 @@ app.get('/', (req, res) => {
    });
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`);
 });
